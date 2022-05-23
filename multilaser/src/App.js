06cb4f37bd7a8feea5d4db43be3f0997922c986e
unknown <cvspinelli09@gmail.com>
2022-05-23 13:54:14 -0300

    fixing erros

--- multilaser/src/App.js
+++ multilaser/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Component } from 'react';
 import Buscador from './component/Buscador/buscador.component';
 import Scroll from './component/Scroll/scroll.component';
