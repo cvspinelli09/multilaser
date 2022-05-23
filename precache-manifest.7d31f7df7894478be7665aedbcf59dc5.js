3285400d5bb031782908099ea763df0dc74626c3
unknown <cvspinelli09@gmail.com>
2022-05-23 13:42:23 -0300

    Updates

---
+++ precache-manifest.7d31f7df7894478be7665aedbcf59dc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fb77757316d0d86c745",
+    "url": "/multilaser/static/css/main.af14cbeb.chunk.css"
+  },
+  {
+    "revision": "4fb77757316d0d86c745",
+    "url": "/multilaser/static/js/main.4fb77757.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/multilaser/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "b93a611dc8c9f5174cb3",
+    "url": "/multilaser/static/js/2.b93a611d.chunk.js"
+  },
+  {
+    "revision": "658e8f12e046f87740b8",
+    "url": "/multilaser/static/js/runtime~main.658e8f12.js"
+  },
+  {
+    "revision": "dea66d5441b5e9cbabe93ef807396867",
+    "url": "/multilaser/index.html"
+  }
+];
